1619b20a99e8d7b170fa072723351291e861ee90
DarkLaughter <ramon.l.echeverria@gmail.com>
2020-10-21 18:36:45 -0400

    text changes

--- src/components/SearchGif.jsx
+++ src/components/SearchGif.jsx
@@ -8,7 +8,7 @@ const SearchGif = () => {
 
     return (
             <div className="container" >
-            <h2>This GIF about {tag}</h2>
+            <h2>This {tag} GIF </h2>
             <h2>is Rated {rating}</h2>
             <img width="500" src={gif} alt="random gif" />
             <input value={tag} onChange={(e) => setTag(e.target.value)} ></input>
